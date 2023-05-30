c47a258b723758fe723a9612d40fb4bd83e4f96a
PriankaMaheru190042112 <priankamaheru@iut-dhaka.edu>
2023-05-31 02:34:19 +0600

    footer component

--- englishshikhii_frontend/src/app/page.tsx
+++ englishshikhii_frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PageNavbar from './header';
 import styles from '../styles/styles.module.css';
+import Footer from '../../components/footer';
 
 
 const Home = () => {
@@ -12,9 +13,11 @@ const Home = () => {
         <h1>Welcome to EnglishShikhii</h1>
         <p>This is the homepage of the EnglishShikhii.</p>
       </main>
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <p>&copy; This is Footer Element. This will be edited.</p>
-      </footer>
+      </footer> */}
+
+<Footer />
     </div>
   );
 };
