bd6e6bb8876cfe8387f04318c499a8f8022e609e
PriankaMaheru190042112 <priankamaheru@iut-dhaka.edu>
2023-06-02 11:02:09 +0600

    header added

---
+++ englishshikhii_frontend/components/header.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from '../src/styles/header.module.css';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>Your Logo</div>
+
+      <div className={styles.frame4623}>
+      <div className={styles.frame7}>
+      <div className={styles.questions}>Questions</div>
+       <button className={styles.loginbtn}> Login</button>
+      </div>
+      <div className={styles.language}>
+        <select>
+          <option value="en">EN</option>
+          <option value="ba">BA</option>
+         
+        </select>
+      </div>
+      </div>
+     
+    
+      
+    </header>
+  );
+};
+
+export default Header;
--- englishshikhii_frontend/src/app/page.tsx
+++ englishshikhii_frontend/src/app/page.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
-import PageNavbar from './header';
 import styles from '../styles/styles.module.css';
 import Footer from '../../components/footer';
-
+import Header from '../../components/header';
 
 const Home = () => {
   return (
     <div className={styles.container}>
-      <PageNavbar/>
+      <Header/>
 
       <main className={styles.mainContent}>
         <h1>Welcome to EnglishShikhii</h1>
         <p>This is the homepage of the EnglishShikhii.</p>
       </main>
-      {/* <footer className={styles.footer}>
-        <p>&copy; This is Footer Element. This will be edited.</p>
-      </footer> */}
 
-<Footer />
+
+     <Footer />
     </div>
   );
 };
